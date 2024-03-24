27acf41e54d06349cf6ebe0e0358823533e09cd1
Tim <ju4nshin@gmail.com>
2024-03-24 14:34:26 +0900

    feat: framer-motion 적용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { FC, ReactNode, useEffect } from "react";
 import TextField from "./components/TextField";
 import FlexBox from "./components/FlexBox";
 import { onlyNumbers } from "./utils/regex";
 import { calcEndTime, calculateTimeLeft, convertTimeToString, convertTo12HourFormat } from "./utils/calculator";
-
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 type Time = {
   hour: number;
   minute: number;
@@ -44,34 +44,39 @@ const App: React.FC = () => {
         value={week}
         onChange={e => setWeek(onlyNumbers(e.target.value))}
       />
-      {time.map((t, index) => (
-        <FlexBox key={index} gap={1}>
-          <TextField
-            label={`${index + 1}주차 시작 시간`}
-            placeholder="09:00"
-            value={t.hour}
-            onChange={e => {
-              setTime(prev => {
-                const newTime = [...prev];
-                newTime[index].hour = Number(e.target.value);
-                return newTime;
-              });
-            }}
-          />
-          <TextField
-            label="분"
-            placeholder="00"
-            value={t.minute}
-            onChange={e => {
-              setTime(prev => {
-                const newTime = [...prev];
-                newTime[index].minute = Number(e.target.value);
-                return newTime;
-              });
-            }}
-          />
-        </FlexBox>
-      ))}
+      <AnimatePresence>
+        {time.map((t, index) => (
+          <Item key={index}>
+            <FlexBox gap={1}>
+              <TextField
+                label={`${index + 1}주차 시작 시간`}
+                placeholder="09:00"
+                value={t.hour}
+                onChange={e => {
+                  setTime(prev => {
+                    const newTime = [...prev];
+                    newTime[index].hour = Number(e.target.value);
+                    return newTime;
+                  });
+                }}
+              />
+              <TextField
+                label="분"
+                placeholder="00"
+                value={t.minute}
+                onChange={e => {
+                  setTime(prev => {
+                    const newTime = [...prev];
+                    newTime[index].minute = Number(e.target.value);
+                    return newTime;
+                  });
+                }}
+              />
+            </FlexBox>
+          </Item>
+        ))}
+      </AnimatePresence>
+
       <button onClick={getTime}>계산하기</button>
       <h3>{!!leftTime && convertTimeToString(leftTime, "hourMinute") + " 남았습니다."}</h3>
       <h3>{!!result && convertTo12HourFormat(result) + "에 퇴근하시면 됩니다!"}</h3>
@@ -80,3 +85,21 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+const Item: FC<{ children: ReactNode; onClick?: () => void }> = ({ children, onClick }) => {
+  const isPresent = useIsPresent();
+  const animations = {
+    style: {
+      position: isPresent ? "static" : ("absolute" as any),
+    },
+    initial: { scale: 0, opacity: 0 },
+    animate: { scale: 1, opacity: 1 },
+    exit: { scale: 0, opacity: 0 },
+    transition: { type: "spring", stiffness: 900, damping: 40 },
+  };
+  return (
+    <motion.div {...animations} layout onClick={onClick}>
+      {children}
+    </motion.div>
+  );
+};
